ad92021f34e52093be03f0e74233d4b8ff335f4e
Jared Norman <jared@creepywizard.com>
2016-01-26 21:51:58 -0800

    Add curses and noodle around with it

--- falling.gemspec
+++ falling.gemspec
@@ -27,6 +27,7 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   spec.add_dependency "thor", "~> 0.19.1"
+  spec.add_dependency "curses", "~> 1.0"
 
   spec.add_development_dependency "pry", "~> 0.10"
   spec.add_development_dependency "bundler", "~> 1.11"
--- lib/falling/cli.rb
+++ lib/falling/cli.rb
@@ -1,6 +1,36 @@
 require 'thor'
+require 'curses'
+
 
 module Falling
   class Cli < Thor
+    desc "start_game", "start a new game of Falling"
+    def start_game()
+      x = 10
+      y = 3
+      Curses.init_screen
+      begin
+        Curses.crmode
+        Curses.noecho
+
+        map = Curses::Window.new(
+          Curses.lines, Curses.cols * 3 / 5,
+          0, 0
+        )
+        map.setpos(0, 0)
+
+        messages = Curses::Window.new(
+          Curses.lines, Curses.cols * 2 / 5,
+          0, Curses.cols * 3 / 5
+        )
+        messages.setpos(0, 0)
+
+        map.noutrefresh
+        messages.noutrefresh
+        Curses.doupdate
+      ensure
+        Curses.close_screen
+      end
+    end
   end
 end
