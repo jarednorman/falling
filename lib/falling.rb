b0fc4bc32181fef2dfd4ebc10d8877c7d5809308
Jared Norman <jared@creepywizard.com>
2016-01-27 21:28:05 -0800

    Add logger

--- lib/falling.rb
+++ lib/falling.rb
@@ -1,4 +1,6 @@
 require 'curses'
+require 'logger'
+require 'tempfile'
 
 require 'falling/version'
 require 'falling/universe'
@@ -8,6 +10,36 @@ module Falling
   class << self
     def start_game
       Interface.new(universe: Falling::Universe.new).run!
+    ensure
+      log_file.close
+      log_file.unlink if log_file.is_a? Tempfile
+    end
+
+    def logger
+      @logger ||=
+        Logger.new(log_file).tap do |logger|
+          logger.level =
+            if development?
+              Logger::DEBUG
+            else
+              Logger::WARN
+            end
+        end
+    end
+
+    private
+
+    def log_file
+      @log_file ||=
+        if development?
+          File.open('./development.log', File::WRONLY | File::APPEND | File::CREAT)
+        else
+          Tempfile.new('falling.log')
+        end
+    end
+
+    def development?
+      ENV['FALLING_ENV'] == 'development'
     end
   end
 end
