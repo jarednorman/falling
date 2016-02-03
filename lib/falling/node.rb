7e6f608bd2cf90bc51da20bb59ea6d0863c5b267
Jared Norman <jared@creepywizard.com>
2016-02-02 22:47:50 -0800

    Implement node storage in universe

--- lib/falling/node.rb
+++ lib/falling/node.rb
@@ -3,19 +3,34 @@ module Falling
     attr_reader :identifier
 
     class << self
+      attr_accessor :universe
+
       def reference(name)
+        variable_name = "@#{name}_identifier"
+
         define_method(name) do
-          instance_variable_get("@#{name}")
+          universe.fetch_node(instance_variable_get(variable_name))
         end
 
         define_method("#{name}=") do |value|
-          instance_variable_set("@#{name}", value)
+          instance_variable_set(variable_name, value.identifier)
         end
       end
     end
 
     def initialize
       @identifier = SecureRandom.uuid
+      universe.add_node(self)
+    end
+
+    def destroy!
+      universe.remove_node(self)
+    end
+
+    private
+
+    def universe
+      Node.universe
     end
   end
 end
--- test/falling/node_test.rb
+++ test/falling/node_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 module Falling
   class NodeTest < Minitest::Test
+    def setup
+      Node.universe = mock_universe
+    end
+
     def test_identifiers_are_unique
       a = Node.new
       b = Node.new
@@ -21,6 +25,35 @@ module Falling
       assert_nil cat.mother
       cat.mother = mom
       assert_equal mom, cat.mother
+    end
+
+    def test_destroyed_node_references_are_nil
+      cat_class = Class.new(Node) { reference :mother }
+      mom = cat_class.new
+      cat = cat_class.new
+      cat.mother = mom
+      mom.destroy!
+      assert_nil cat.mother
+    end
+
+    private
+
+    def mock_universe
+      Object.new.tap do |universe|
+        nodes = {}
+
+        universe.define_singleton_method(:fetch_node) do |identifier|
+          nodes[identifier]
+        end
+
+        universe.define_singleton_method(:add_node) do |node|
+          nodes[node.identifier] = node
+        end
+
+        universe.define_singleton_method(:remove_node) do |node|
+          nodes.delete(node.identifier)
+        end
+      end
     end
   end
 end
